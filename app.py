ba0755c0bd9aa4196ecbccaa1690f905c24b8085
Ryan Morgan <R.Morgan5399@gmail.com>
2023-03-26 19:57:26 -0700

    Add route to handle login functionality

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route("/login", methods=['GET'])
+def index():
+    return render_template('login.html')
